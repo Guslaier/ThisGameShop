b933d0c6515264bb4077e76dee979a55f1ccc57c
Guslaier <goolzxcv@gmail.com>
2025-09-25 23:05:51 +0700

    v.2 awewae

--- public/javascripts/store.js
+++ public/javascripts/store.js
@@ -1,26 +1,32 @@
 
+let container = document.getElementById("ShopExplo");
 document.querySelectorAll('.layout').forEach(d => {
     d.addEventListener("click", (e) => {
         const el = e.currentTarget;
         if (el.getAttribute("active") === "False") {
+            container.classList.toggle("horizontal");
+            container.classList.toggle("normal");
             el.setAttribute("active", "True");
             if (el.getAttribute("id") === "gitQ") {
                 document.getElementById("gitW").setAttribute("active", "False");
             }
             if (el.getAttribute("id") === "gitW") {
                 document.getElementById("gitQ").setAttribute("active", "False");
-            } 
+            }
         } else if (el.getAttribute("active") != "True") {
             el.setAttribute("active", "False");
         }
     });
 });
 
-let container = document.getElementById("ShopExplo");
+
 let g = container.firstElementChild;
 
 for (let i = 0; i < 100; i++) {
-  let clone = g.cloneNode(true);
-  container.appendChild(clone);
+    let clone = g.cloneNode(true);
+    container.appendChild(clone);
 }
+
+
+
 
